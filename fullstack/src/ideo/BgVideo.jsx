3f747d587469e5dff0a20b9e70feb3c4a3ac2145
Meaga J <119093848+jmeaga@users.noreply.github.com>
2024-02-15 12:40:48 +0530

    Delete fullstack directory

--- fullstack/src/ideo/BgVideo.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function BgVideo(){
-    return 
-}
-export default BgVideo;
--- fullstack/src/main/java/com/example/demo/enumerated/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.enumerated;
-
-public enum Role {
-    USER, ADMIN;
-}
